6c60789e5e7d94bd5729cca4209bd445efe71644
Aaghash R <aaghash.r@in.abb.com>
2024-03-09 16:05:05 +0530

    first commit

---
+++ src/login.jsx
@@ -0,0 +1,65 @@
+import { useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
+import './login.css'
+
+
+function Login() {
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handleSubmit = (event) => {
+        event.preventDefault(); 
+        console.log("Username:", username);
+        console.log("Password:", password);
+    };
+    return (
+        <div className="login-container">
+          <form onSubmit={handleSubmit}>
+            <div className="mb-3">
+              <h3 className="text-center">Event Name</h3>
+            </div>
+            <div className="mb-4">
+              <h3 className="text-center">Login</h3>
+            </div>
+            <div className="mb-3">
+              <label htmlFor="username" className="form-label">
+                Username
+              </label>
+              <input
+                type="text"
+                className="form-control"
+                name="username"
+                placeholder="Enter your username"
+                value={username}
+                onChange={(event) => setUsername(event.target.value)}
+                required
+              />
+            </div>
+            <div className="mb-3">
+              <label htmlFor="password" className="form-label">
+                Password
+              </label>
+              <input
+                type="password"
+                className="form-control"
+                name="password"
+                placeholder="Enter your password"
+                value={password}
+                onChange={(event) => setPassword(event.target.value)} 
+                required
+              />
+            </div>
+            <div className="text-center">
+              <button type="submit" className="btn btn-primary">
+                Login
+              </button>
+            </div>
+            <br></br>
+            <p className="w-100 text-center">Not Registered? Sign Up</p>
+          </form>
+        </div>
+      );
+}
+
+export default Login;
