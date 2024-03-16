6e560dc33c22bb11550ff0d1100b29f71b607b14
Aaghash R <aaghash.r@in.abb.com>
2024-03-16 08:43:12 +0530

    push

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
-import Login from './login'; 
-import Admin from './admin';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
+import "./css/my.css";
+
+import Admin from "./pages/admin";
+import Login from "./pages/login";
 
 function App() {
   return (
-    <div>
-      <Login/>
+    <div className="App">
       <Admin />
+      <Login />
     </div>
   );
 }
